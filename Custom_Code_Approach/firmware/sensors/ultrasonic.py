5f3dcc6766e88090fd20e6b4efabc36cbd1bf989
Kiongo-bob <njoguub@gmail.com>
2026-07-25 13:57:15 +0300

    Added exception handling

--- Custom_Code_Approach/firmware/sensors/ultrasonic.py
+++ Custom_Code_Approach/firmware/sensors/ultrasonic.py
@@ -1,14 +1,17 @@
-import machine, time
+import time
 from machine import Pin
 from hcsr04 import HCSR04
 
 trigger_pin = 15
-echo_pin = 0
+echo_pin = 14
+
+trig_status =  Pin(16, Pin.IN, Pin.PULL_DOWN) # Check the status of the trigger pin
 
 sensor = HCSR04(trigger_pin, echo_pin)
 
-distance = sensor.distance_cm()
-
-while distance:
-    # Implement a PIR sensor since it draws very little current to wake the trigger Pin
-    pass
+while true:
+    try:
+        distance = sensor.distance_cm() # reads distance in every iteration
+        print(f'{:.2f} cm'.format(distance))
+    except OSError as e:
+        print("Echo pin input not received: ", e)
